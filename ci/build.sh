13b5e2e2f1aa52f0bf693ba5c6b3fc63b991270c
Max <max@maxlab.io>
2024-08-19 20:27:08 -0700

    workflow: another attempt to invoke IDF

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
 # TODO: print more information about the container
 
 (ls /.dockerenv && echo Found dockerenv) || (echo No dockerenv)
-echo "IDF: $IDF_PATH"
 echo "IDF_PATH: $IDF_PATH"
 echo "PATH: $PATH"
 
-source "$IDF/export.sh"
+. "${IDF_PATH}/export.sh"
 idf.py build
