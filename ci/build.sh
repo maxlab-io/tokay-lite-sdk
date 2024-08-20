306c6db2b326e19358b787be47a135c6a270da44
Max <max@maxlab.io>
2024-08-19 20:05:21 -0700

    address #5: add prelim GH actions scripts and configs
    Docker is selected as a convinient way to build the thing

---
+++ ci/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TODO: print more information about the container
+
+(ls /.dockerenv && echo Found dockerenv) || (echo No dockerenv)
+echo "IDF_PATH: $IDF_PATH"
+echo "PATH: $PATH"
+
+. "${IDF_PATH}/export.sh"
+idf.py -C webserver build
